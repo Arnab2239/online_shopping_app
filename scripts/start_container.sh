8d1ff298bdf4900b467eddee4d3e6561655895a0
Arnab2239 <155752433+Arnab2239@users.noreply.github.com>
2025-08-06 19:22:07 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@
   docker pull arnab23/tws-online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 arnab23/tws-online-shop-app-demo:latest
