78ef74d32f4dc9ff490640b9bd373d6a20fa978a
Arnab2239 <155752433+Arnab2239@users.noreply.github.com>
2025-08-06 22:55:04 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-
-# Update package list
-apt update -y
-
-# Install Docker
-apt install -y docker.io
-
-# Start Docker service
-systemctl start docker
-systemctl enable docker
-
-# Optional: Add 'ubuntu' user to docker group (if using non-root runs)
-usermod -aG docker ubuntu
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
